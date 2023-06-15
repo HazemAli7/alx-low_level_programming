941f578d86fb5cb27f5dc840efb06ad61c46b4df
HazemAli7 <hazemaligalal@outlook.com>
2023-06-15 14:41:39 -0700

    HAZEMALi7

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:A C program that prints with put function
+ * Return: Alwys 0(Success)
+ */
+
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
