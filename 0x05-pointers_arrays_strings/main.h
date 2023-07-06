4496170d095b5b6b5850026fe77677054273f487
HazemAli7 <hazemaligalal@outlook.com>
2023-07-06 08:48:18 -0700

    HAZEM

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,4 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /*MAIN_H */
+#endif /* MAIN_H */
