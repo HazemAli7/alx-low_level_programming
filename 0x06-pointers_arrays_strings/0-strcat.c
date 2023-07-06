c45c5102f73b3414542a1d7cb6d2d36a58ae18d1
HazemAli7 <hazemaligalal@outlook.com>
2023-07-06 11:25:43 -0700

    HAZEM

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,16 @@
-#include <main.h>
+#include <stdio.h>
 
 /**
- * _strcst - function that concatinations
- * 		two strings
+ * _strcat - function that concatenates
+ *          two strings.
  *
- * @dest: pointer to destination input
+ * @dest: pointer to destnation input
  * @src: pointer to source input
  *
  * Return: pointer to resulting string @dest
- */
+*/
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int c, c2;
 
@@ -21,7 +21,7 @@ char *_strcat(char *dest, char *src);
 
 	/* iterate through each src array value without the null byte*/
 	for (c2 = 0; src[c2] ; c2++)
-		/* append src[c2] to dest[c] while overwritting the null byte in dest*/
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
 		dest[c++] = src[c2];
 
 	return (dest);
